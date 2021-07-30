f76036daac10512a159466e4593e36473c837a70
Nam Phương Mỹ Nhân Trà <69616050+MissTea-VN@users.noreply.github.com>
2021-07-31 03:01:36 +0700

    create github pages

--- assets/witsec-mailform/mailform.js
+++ assets/witsec-mailform/mailform.js
@@ -3,6 +3,13 @@ function witsecSendMail(obj) {
 
     var frm = $(obj);
 
+    // Check if site uses Captcha
+	if (frm.find("input[name='captcha']").length && !frm.find("input[name='captcha']").hasClass("is-valid")) {
+        console.error("Not sending the form, because the Captcha is incorrect.");
+    	frm.find(".captcha").focus();
+        return false;
+    }
+
     // Check if a reCAPTCHA site key is defined (v3)
     if (typeof witsecRcpSitekey !== 'undefined') {
         grecaptcha.ready(function() {
@@ -31,10 +38,13 @@ function witsecSendMailAjax(frm) {
     if (frm.find("input[name=disableButton]").val() == "1")
         frm.find("button[type=submit]").attr("disabled", true);
 
+    // Let's try to send the form
     $.ajax({
         type: frm.attr("method"),
         url: frm.attr("action"),
-        data: frm.serialize(),
+        data:        ( typeof FormData !== "undefined" ? new FormData(frm[0]) : frm.serialize() ), // Ancient browsers can't handle FormData (file uploads won't work, but the form is still sent)
+        contentType: ( typeof FormData !== "undefined" ? false : "application/x-www-form-urlencoded; charset=UTF-8" ),
+        processData: ( typeof FormData !== "undefined" ? false : true ),
         success: function (data) {
             if (data.success) {
                 // Check if we need to redirect or not
@@ -43,12 +53,15 @@ function witsecSendMailAjax(frm) {
                 } else {
                     frm.find("div .alert-danger").attr("hidden", "hidden");
                     frm.find("div .alert-success").removeAttr("hidden");
+                    frm.find("input[name='captcha']").removeClass("is-valid is-invalid");
+                    frm.find(".captcha").css("background-image", "url(assets/witsec-mailform/captcha.php" + "?" + Date.now() + ")");
                     frm.trigger("reset");
                 }
             } else {
                 frm.find("div .alert-success").attr("hidden", "hidden");
                 frm.find("div .alert-danger").removeAttr("hidden");
-                console.error("Mailform: An error occured. " + data.message);
+                witsecMfSetErrors(frm.find("div .alert-danger"), data);
+                console.error("Mailform error: [" + data.errorcode + "] " + data.message);
             }
 
             // Enable submit button
@@ -59,9 +72,95 @@ function witsecSendMailAjax(frm) {
 
             frm.find("div .alert-success").attr("hidden", "hidden");
             frm.find("div .alert-danger").removeAttr("hidden");
+            witsecMfSetErrors(frm.find("div .alert-danger"), false);
 
             // Enable submit button
             frm.find("button[type=submit]").attr("disabled", false);
         },
     });
 }
+
+var wsMfErrorOrg = "";
+function witsecMfSetErrors(div, data) {
+    // Default error values
+    let m = "Something went wrong somewhere.";
+    let e = "E1201";
+
+    // Save the original error message, in case another error occurs
+    if (wsMfErrorOrg)
+        div.html(wsMfErrorOrg);
+    else
+        wsMfErrorOrg = div.html();
+
+    if (data) {
+        e = data.errorcode;
+        m = (typeof wsMfErrors !== "undefined" && wsMfErrors[data.errorcode] ? wsMfErrors[data.errorcode] : data.message);
+    }
+
+    // Set error code and message
+    div.text(function(index, text) {
+        return text.replace("{errorcode}", e).replace("{errormsg}", m);
+    });
+}
+
+// Valide the captcha
+function witsecValidateCaptcha(f) {
+	f = $(f);
+
+    $.ajax({
+        type: "get",
+        url: "assets/witsec-mailform/captcha.php?check=" + f.val(),
+        success: function (data) {
+            if (data.success) {
+                f.removeClass("is-invalid").addClass("is-valid");
+            } else {
+                f.removeClass("is-valid").addClass("is-invalid");
+                console.error("Captcha incorrect");
+            }
+        },
+        error: function (data) {
+            console.error("Captcha failed");
+            f.removeClass("is-valid").addClass("is-invalid");
+        },
+    });
+}
+
+// Listeners
+document.addEventListener("DOMContentLoaded", function(event) {
+    // When someone entered a CAPTCHA code
+    $(".witsec-mailform form input[name='captcha']").change(function() {
+        var f = $(this);
+
+        $.ajax({
+            type: "get",
+            url: "assets/witsec-mailform/captcha.php?check=" + f.val(),
+            success: function (data) {
+                if (data.success) {
+                    f.removeClass("is-invalid").addClass("is-valid");
+                } else {
+                    f.removeClass("is-valid").addClass("is-invalid");
+                    console.error("Captcha incorrect");
+                }
+            },
+            error: function (data) {
+                console.error("Captcha failed");
+                f.removeClass("is-valid").addClass("is-invalid");
+            },
+        });
+    });
+
+    // When CAPTCHA reload has been clicked
+    $(".witsec-mailform form .captchaReload").click(function() {
+        let p = $(this).closest("form");
+        if (p) {
+            p.find(".captcha").css("background-image", "url(assets/witsec-mailform/captcha.php" + "?" + Date.now() + ")");
+            p.find("input[name='captcha']").removeClass("is-valid is-invalid");
+            p.find("input[name='captcha']").val("");
+        }
+    });
+
+    // Listen for form submits
+    $(".witsec-mailform form").not("[onsubmit]").submit(function(event) {
+        witsecSendMail( $(this)[0] );
+    });
+});
